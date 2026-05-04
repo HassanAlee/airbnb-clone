5325b09e2ea5fff6a904f1386e1c3de444b9bf4b
HassanAlee <hassanalics72@gmail.com>
2026-05-04 18:55:09 +0500

    feat: integrate Prisma ORM and supabase and implement user synchronization auth route

---
+++ app/api/auth/create/route.ts
@@ -0,0 +1,26 @@
+import { prisma } from "@/lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  if (!user) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+  let dbUser = await prisma.user.findUnique({ where: { id: user.id } });
+  if (!dbUser) {
+    dbUser = await prisma.user.create({
+      data: {
+        email: user.email!,
+        id: user.id,
+        firstName: user.given_name!,
+        lastName: user.family_name!,
+        profileImage:
+          user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
+      },
+    });
+  }
+
+  return NextResponse.redirect("http://localhost:3000");
+}
