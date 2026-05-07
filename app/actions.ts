c5c1d211e2f8f1f9d794c05a81e8de77274e50a6
HassanAlee <hassanalics72@gmail.com>
2026-05-07 08:27:37 +0500

    feat: implement property description creation page with form inputs and shared counter component, added supabase and storing images

--- app/actions.ts
+++ app/actions.ts
@@ -2,6 +2,7 @@
 
 import { prisma } from "@/lib/db";
 import { redirect } from "next/navigation";
+import { supabase } from "./lib/supabase";
 
 // create home
 export async function createAirbnbHome({ userId }: { userId: string }) {
@@ -49,4 +50,47 @@ export async function createCategory(formData: FormData) {
     },
   });
   return redirect(`/create/${homeId}/description`);
+}
+
+// create description
+export async function createDescription(formData: FormData) {
+  const homeId = formData.get("homeId") as string;
+  const title = formData.get("title") as string;
+  const description = formData.get("description") as string;
+  const price = formData.get("price");
+  const image = formData.get("image") as File;
+  const guest = formData.get("guest") as string;
+  const room = formData.get("room") as string;
+  const bathroom = formData.get("bathroom") as string;
+  let imageUrl: string = "";
+  if (image) {
+    const { data } = await supabase.storage
+      .from("images")
+      .upload(`images/${image.name}-${new Date()}`, image, {
+        cacheControl: "2592000",
+        contentType: "image/png",
+      });
+    // TODO: implement proper error handling for images
+    // if (error) {
+    //   throw error;
+    // }
+    imageUrl = data?.path as string;
+  }
+
+  await prisma.home.update({
+    where: {
+      id: homeId,
+    },
+    data: {
+      title: title,
+      description: description,
+      price: Number(price),
+      guests: guest,
+      bedrooms: room,
+      bathrooms: bathroom,
+      photo: imageUrl,
+      addedDescription: true,
+    },
+  });
+  return redirect(`/create/${homeId}/location`);
 }
